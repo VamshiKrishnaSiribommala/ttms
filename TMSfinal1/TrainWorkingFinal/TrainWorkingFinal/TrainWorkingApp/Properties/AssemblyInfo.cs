87e3d3f94fdd338831ed15a3640d899d02987870
VamshiKrishnaSiribommala <vamshikrishnasiribommala@gmail.com>
2026-09-08 23:54:47 +0530

    Commit 94/105: add AssemblyInfo.cs

---
+++ TMSfinal1/TrainWorkingFinal/TrainWorkingFinal/TrainWorkingApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Train Working Management System (TMS)")]
+[assembly: AssemblyDescription("Indian Railways Station Operational Authorities & Train Working System")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Indian Railways")]
+[assembly: AssemblyProduct("TrainWorkingApp")]
+[assembly: AssemblyCopyright("Copyright © Indian Railways 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8B321940-8C8E-45E9-8239-1E1C55353591")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
